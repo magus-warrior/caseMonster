d986cf9bdb9134dd466b5aa562971f518ca71d3a
magus-warrior <82559270+magus-warrior@users.noreply.github.com>
2025-09-28 21:27:42 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,139 @@
+import pyperclip
+import pyautogui as pya
+import time
+
+
+def funky(text):
+    #split_text = [i for i in text]
+    #split_text = cap_first_letter(split_text)
+
+    return cap_sentences(text)
+
+
+def cap_sentences(lst):
+    #sentences = "".join(lst).split(".")
+    sentences = lst.split(".")
+    new_lst = []
+
+    for sentence in sentences:
+        split_sentence = [i for i in sentence]
+        print(f"Sentence: {sentence} - Split: {split_sentence}")
+        new_lst.append("".join(cap_first_letter(split_sentence)))
+        
+    print(f"new list -1 : {new_lst[-1]}")
+        
+    
+
+    return cap_special(".".join(new_lst).strip())
+
+
+def cap_special(text):
+    print(text)
+    split = [i for i in text]
+    fin = []
+    caps =False
+    
+    for char in split:
+        print(char)
+        if char in ['\t','\n','\r']:
+            fin.append(char)
+            caps = True
+        else:
+            if caps is True:
+                try:
+                    fin.append(char.upper())
+                    caps = False
+                except:
+                    fin.append(char)
+                
+            else:
+                fin.append(char)    
+                    
+    return "".join(fin).strip()
+            
+
+def cap_first_letter(lst):
+    fin_list = []
+    symbols = ["!", "?"]
+    caps = True
+
+    for index, char in enumerate(lst):
+
+        if char == " ":
+            fin_list.append(char)
+
+        elif char in symbols:
+            fin_list.append(char)
+            caps = True
+
+        elif char.isalpha() is True and caps == True:
+            fin_list.append(char.upper())
+            caps = False
+
+
+        elif char.isalpha() is True:
+
+            if(char.lower() == "i" and fin_list[-1] == " " and lst[index+1] == " "):
+                fin_list.append(char.upper())
+                print(f"CHAR I - {char}")
+
+
+            elif(char.lower() == "i" and lst[index+1] in ["’","'"] and index != len(lst)):
+                fin_list.append(char.upper())
+                print(f"CHAR with apostrophe {char}")
+                
+
+            else:
+                fin_list.append(char.lower())
+
+
+        else:
+            fin_list.append(char.lower())
+
+    return fin_list
+
+
+def upper_case():
+    pya.hotkey('alt', 'tab')
+    time.sleep(.01)
+    pyperclip.copy("")
+    pya.hotkey('ctrl', 'c')
+    time.sleep(.01)
+    pyperclip.copy(pyperclip.paste().upper())
+    pya.hotkey('ctrl', 'v')
+    time.sleep(.01)
+    print("Upper ran")
+
+
+def lower_case():
+    pya.hotkey('alt', 'tab')
+    time.sleep(.01)
+    pyperclip.copy("")
+    pya.hotkey('ctrl', 'c')
+    time.sleep(.01)
+    pyperclip.copy(pyperclip.paste().lower())
+    pya.hotkey('ctrl', 'v')
+    time.sleep(.01)
+
+
+def title_case():
+    pya.hotkey('alt', 'tab')
+    time.sleep(.01)
+    pyperclip.copy("")
+    pya.hotkey('ctrl', 'c')
+    time.sleep(.01)
+    pyperclip.copy(pyperclip.paste().title())
+    pya.hotkey('ctrl', 'v')
+    time.sleep(.01)
+
+
+def funky_case():
+    pya.hotkey('alt', 'tab')
+    time.sleep(.01)
+    pyperclip.copy("")
+    pya.hotkey('ctrl', 'c')
+    time.sleep(.01)
+    text = funky(pyperclip.paste().upper())
+    pyperclip.copy(text)
+    pya.hotkey('ctrl', 'v')
+    time.sleep(.01)
---
+++ window.py
@@ -0,0 +1,134 @@
+import os
+
+from main import lower_case, title_case, upper_case, funky_case
+
+# -*- coding: utf-8 -*-
+
+###########################################################################
+## Python code generated with wxFormBuilder (version 3.10.1-34-g2d20e717)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO *NOT* EDIT THIS FILE!
+###########################################################################
+
+import wx
+import wx.xrc
+
+
+###########################################################################
+## Class MONSTERcase
+###########################################################################
+
+class MONSTERcase(wx.Frame):
+
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=u"WarpTyme - CASEmonster", pos=wx.DefaultPosition,
+                          size=wx.Size(350, 400), style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
+
+        self.SetSizeHints(wx.DefaultSize, wx.DefaultSize)
+
+        bSizer1 = wx.BoxSizer(wx.VERTICAL)
+
+        self.m_bitmap1 = wx.StaticBitmap(self, wx.ID_ANY, wx.Bitmap(u"logo.png", wx.BITMAP_TYPE_ANY),
+                                         wx.DefaultPosition, wx.DefaultSize, 0)
+        bSizer1.Add(self.m_bitmap1, 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALL, 5)
+
+        bSizer2 = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.upperButton = wx.Button(self, wx.ID_ANY, u"Upper", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.upperButton.SetFont(
+            wx.Font(9, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False, "Arial"))
+
+        bSizer2.Add(self.upperButton, 1, wx.ALL, 5)
+
+        bSizer1.Add(bSizer2, 1, wx.EXPAND, 5)
+
+        bSizer21 = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.titleButton = wx.Button(self, wx.ID_ANY, u"Title", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.titleButton.SetFont(
+            wx.Font(9, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False, "Arial"))
+
+        bSizer21.Add(self.titleButton, 1, wx.ALL, 5)
+
+        bSizer1.Add(bSizer21, 1, wx.EXPAND, 5)
+
+        bSizer211 = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.lowerButton = wx.Button(self, wx.ID_ANY, u"Lower", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.lowerButton.SetFont(
+            wx.Font(9, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False, "Arial"))
+
+        bSizer211.Add(self.lowerButton, 1, wx.ALL, 5)
+
+        bSizer1.Add(bSizer211, 1, wx.EXPAND, 5)
+
+        bSizer2111 = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.funkyButton = wx.Button(self, wx.ID_ANY, u"Sentence", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.funkyButton.SetFont(
+            wx.Font(9, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_ITALIC, wx.FONTWEIGHT_HEAVY, False, "Arial Black"))
+
+        bSizer2111.Add(self.funkyButton, 1, wx.ALL, 5)
+
+        bSizer1.Add(bSizer2111, 1, wx.EXPAND, 5)
+
+        self.SetSizer(bSizer1)
+        self.Layout()
+        self.m_menubar4 = wx.MenuBar(0)
+        self.m_menu1 = wx.Menu()
+        self.m_menuItem1 = wx.MenuItem(self.m_menu1, wx.ID_ANY, u"How to use", wx.EmptyString, wx.ITEM_NORMAL)
+        self.m_menu1.Append(self.m_menuItem1)
+
+        self.m_menubar4.Append(self.m_menu1, u"Help")
+
+        self.SetMenuBar(self.m_menubar4)
+
+        self.Centre(wx.BOTH)
+
+        # Connect Events
+        self.upperButton.Bind(wx.EVT_BUTTON, self.upperButtonOnButtonClick)
+        self.titleButton.Bind(wx.EVT_BUTTON, self.titleButtonOnButtonClick)
+        self.lowerButton.Bind(wx.EVT_BUTTON, self.lowerButtonOnButtonClick)
+        self.funkyButton.Bind(wx.EVT_BUTTON, self.funkyButtonOnButtonClick)
+        self.Bind(wx.EVT_MENU, self.m_menuItem1OnMenuSelection, id=self.m_menuItem1.GetId())
+
+    def __del__(self):
+        pass
+
+    # Virtual event handlers, override them in your derived class
+
+
+    def upperButtonOnButtonClick(self, event):
+        upper_case()
+        event.Skip()
+
+
+    def lowerButtonOnButtonClick(self, event):
+        lower_case()
+        event.Skip()
+
+
+    def titleButtonOnButtonClick(self, event):
+        title_case()
+        event.Skip()
+
+
+    def funkyButtonOnButtonClick(self, event):
+        funky_case()
+        event.Skip()
+
+
+    def m_menuItem1OnMenuSelection(self, event):
+        os.system("start help.txt")
+        event.Skip()
+
+
+if __name__ == "__main__":
+    app = wx.App()
+    frame = MONSTERcase(None)
+    frame.Show(True)
+    app.MainLoop()
+
+
+
